041a6fcecfa550590ec63ad933ff31b96ed57c61
Sharjeel Aslam <sharjeel.aslam@tkxel.com>
2012-10-23 12:08:22 +0600

    Update app/models/tenant.rb

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -5,16 +5,16 @@ class Tenant < ActiveRecord::Base
   after_create :prepare_tenant
 
   private
-
   def prepare_tenant
     create_schema
     load_tables
   end
 
+  # This function will create create schema for new subdmain.
   def create_schema
     PgTools.create_schema id unless PgTools.schemas.include? id
   end
-
+  # This function will use schema.rb and will load scchema tables into the database.
   def load_tables
     return if Rails.env.test?
     PgTools.set_search_path id, false
